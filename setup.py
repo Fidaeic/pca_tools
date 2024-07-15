7a5776989f3c167196d02355cee7985f55a82d65
Fidaeic <elmorer.fidae@gmail.com>
2024-07-15 18:12:51 +0200

    Updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setup(
     name='pca_tools',
-    version='0.2.2',
+    version='0.2.3',
     description='PCA tools for data analysis',
     long_description=long_description,
     long_description_content_type='text/markdown',    
@@ -22,7 +22,8 @@ setup(
         'scikit-learn',
         'altair',
         'statsmodels',
-        'scipy'
+        'scipy',
+        'ray'
     ],
     test_suite='tests',  # Path to the test suite
     tests_require=[
